317c37d412c8944d4b481966c7d640a8278627f5
tjs- <tjschaap@gmail.com>
2014-10-04 14:16:01 +0400

    3rd rev, for challenge

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -32,7 +33,13 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = "-"+enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+
+        enterNameTextField.text = ""
         enterMessageTextField.text = ""
+        
         enterMessageTextField.resignFirstResponder()
         
         mailButton.backgroundColor = UIColor.grayColor()
